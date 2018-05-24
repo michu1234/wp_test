bf48f2f735f2ff98e58af51700f297a6a118747b
unknown <dawiddeveloper@gmail.com>
2018-05-24 14:22:50 +0200

    employee custom post almost ready

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -21,4 +21,46 @@ function my_acf_google_map_api( $api ){
 }
 
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
+function create_post_type() {
+  register_post_type( 'acme_product',
+    array(
+      'labels' => array(
+        'name' => __( 'Products' ),
+        'singular_name' => __( 'Product' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+    )
+  );
+}
+add_action( 'init', 'create_post_type' );
+
+function employee_init() {
+    $args = array(
+      'label' => 'Employee',
+        'public' => true,
+        'show_ui' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'rewrite' => array('slug' => 'employee'),
+        'query_var' => true,
+        'menu_icon' => 'dashicons-universal-access',
+        'menu_icon' => 'dashicons-universal-access',
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'trackbacks',
+            'custom-fields',
+            'comments',
+            'revisions',
+            'thumbnail',
+            'author',
+            'page-attributes',)
+        );
+    register_post_type( 'employee', $args );
+}
+add_action( 'init', 'employee_init' );
+
 ?>
